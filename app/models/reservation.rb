4e5228ad226065f0d2d8adbf407d276f3b448f1b
Soulou <leo.unbek@gmail.com>
2012-03-29 21:43:37 +0200

    Correction des validators, ils ne doivent pas renvoyer un booléent, mais placer une erreur dans record.error

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,35 +1,27 @@
 class ReservationUniquenessValidator < ActiveModel::Validator
   def validate(record)
-    return Reservation.where(date: record.date, ship_id: record.ship_id, location_id: record.location_id).first == nil
+    if(record.ship == nil or record.location == nil)
+      record.errors[:base] << "The ship or the location does not exist"
+    elsif Reservation.where(date: record.date.beginning_of_day, slice: record.slice, ship_id: record.ship_id, location_id: record.location_id).first != nil
+      record.errors[:base] << "This ship has already booked the place at that time"
+    end
   end
 end
 
 class LocationCapacityTravelersValidator < ActiveModel::Validator
   def validate(record)
     if(record.ship == nil or record.location == nil)
-      return false;
-    end
-    record.ship.travelers <= record.location.capacity
-  end
-end
-
-class AntiDuplicationValidator < ActiveModel::Validator
-  def validate(record)
-    if(record.ship = nil or record.location == nil)
-      return false
-    end
-
-    Reservation.where(ship: record.ship).to_a.each do |s|
-      if record.date.beginning_of_day == s.date.beginning_of_day
-        and record.slice == s.slice then
-        return false
-      end
+      record.errors[:base] << "The ship or the location does not exist"
+    elsif(record.ship.travelers > record.location.capacity)
+      record.errors[:base] << "Too many travalers on that boat"
     end
   end
 end
 
 class Reservation
   include Mongoid::Document
+
+  before_save { |res| res.date = res.date.beginning_of_day }
 
   field :date, type: Time
   # slice of the day 1 < slice < 5
@@ -48,6 +40,5 @@ class Reservation
 
   validates_with ReservationUniquenessValidator 
   validates_with LocationCapacityTravelersValidator
-  validates_with AntiDuplicationValidator
 end
 
